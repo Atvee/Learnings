6db8d25a13452806ad9a7a83d7d966d142ac5650
Atvee <79842115+Atvee@users.noreply.github.com>
2024-12-13 10:55:17 +0530

    Update ToDoList.sol

--- ToDoList.sol
+++ ToDoList.sol
@@ -4,29 +4,30 @@ pragma solidity 0.8.26;
 contract ToDoList {
 
     struct ToDo {
-        string Text;
+        string task;
         bool completed;
     }
 
     ToDo[] public ToDos;
 
-    function MakeList(string calldata task) public  {
+    function MakeList(string calldata _task) public  {
         ToDos.push(ToDo({
-            Text: task,
+            task: _task,
             completed: false
         }));
     }
 
     function updateList(uint _index, string calldata newTask) public {
-        ToDos[_index].Text = newTask;
+        ToDos[_index].task = newTask;
     }
 
     function toggleCompleted(uint _index) public {
         ToDos[_index].completed = !ToDos[_index].completed;
     }
     
-    function get(uint _index) external view returns (string memory) {
-        return ToDos[_index].Text;
+    function get(uint _index) external view returns (string memory, bool ) {
+        ToDo storage todo = ToDos[_index];
+        return (todo.task, todo.completed);
     }
 
 
